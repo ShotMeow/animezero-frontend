c18570cdc0a6b198feeacacc060c1aa25cde5c10
ShotMeow <shotmeow@gmail.com>
2022-07-30 17:45:33 +0300

    Add UI Tag Component

---
+++ app/components/ui/Tag/Tag.tsx
@@ -0,0 +1,24 @@
+import { FC } from 'react'
+
+import styles from './Tag.module.scss'
+import cn from 'classnames'
+
+const Tag: FC<{ title: number | string; black?: boolean; bold?: boolean }> = ({
+	title,
+	black,
+	bold
+}) => {
+	return (
+		<div
+			className={cn(
+				styles.tag,
+				{ [styles.bold]: bold },
+				{ [styles.black]: black }
+			)}
+		>
+			{title}
+		</div>
+	)
+}
+
+export default Tag
