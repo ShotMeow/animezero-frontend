696360ec05eadf30713f07568b87ac581b6758e6
ShotMeow <shotmeow@gmail.com>
2022-08-01 19:43:14 +0300

    Fix Films Api helpers

--- app/services/films.interface.ts
+++ app/services/films.interface.ts
@@ -1,6 +1,16 @@
 export type BlockTypes = 'best' | 'newest' | 'ongoing' | 'recommended'
 
-export interface IFilm {
+interface IFilmType {
+	id: number
+	name: string
+	value: string
+}
+
+export interface IFilm extends IBaseFilm {
+	type?: IFilmType
+}
+
+export interface IBaseFilm {
 	id: number
 	playerLink: string
 	title: string
@@ -18,5 +28,9 @@ export interface IHomePage {
 	best: IFilm[]
 	newest: IFilm[]
 	ongoing: IFilm[]
-	recommended: IFilm[]
+	recommended: IBaseFilm[]
+}
+
+export interface ICurrentFilm {
+	film: IFilm
 }
--- app/services/films.service.ts
+++ app/services/films.service.ts
@@ -4,5 +4,9 @@ import { BlockTypes } from '@/services/films.interface'
 export const FilmsService = {
 	async getAll(blocks: BlockTypes[]) {
 		return axiosClassic.get(`/homepage?blocks=${blocks.toString()}`)
+	},
+
+	async getById(id: number) {
+		return axiosClassic.get(`/film/${id}`)
 	}
 }
