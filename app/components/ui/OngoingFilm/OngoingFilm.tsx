1df70b0203ab6ba1de99b74942e30ef16c262e1c
ShotMeow <shotmeow@gmail.com>
2022-08-01 19:40:51 +0300

    Finish OngoingFilm Card

--- app/components/ui/OngoingFilm/OngoingFilm.tsx
+++ app/components/ui/OngoingFilm/OngoingFilm.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react'
 import { IOngoingFilm } from '@/components/ui/OngoingFilm/Ongoing.interface'
 import styles from './OngoingFilm.module.scss'
 import Button from '@/components/ui/Button/Button'
-import { ageCompileHelper } from '../../../helpers/age-compile.helper'
+import { ageCompileHelper } from '@/helpers/age-compile.helper'
 const OngoingFilm: FC<IOngoingFilm> = ({
 	title,
 	imageUrl,
@@ -10,6 +10,7 @@ const OngoingFilm: FC<IOngoingFilm> = ({
 	year,
 	ganres
 }) => {
+	const currentGanres = [ganres[0], ganres[1], ganres[2]]
 	return (
 		<article className={styles.ongoing_film}>
 			<img src={imageUrl} alt={title} />
@@ -19,17 +20,24 @@ const OngoingFilm: FC<IOngoingFilm> = ({
 				</h3>
 				<p className={styles.ganres}>
 					<span>Жанры: </span>
-					{ganres.map(ganre => (
-						<a key={ganre} href='#'>
-							{' '}
-							{ganre}
-						</a>
-					))}
+					{currentGanres.map(ganre =>
+						ganre === currentGanres[currentGanres.length - 1] ? (
+							<a key={ganre} href='#'>
+								{' '}
+								{ganre}
+							</a>
+						) : (
+							<a key={ganre} href='#'>
+								{' '}
+								{ganre},
+							</a>
+						)
+					)}
 				</p>
 				<p className={styles.ages}>
 					Возрастной рейтинг: <span>{ageCompileHelper(minimalAge)}</span>
 				</p>
-				<Button>Отслеживать</Button>
+				<Button important='primary'>Отслеживать</Button>
 			</div>
 		</article>
 	)
