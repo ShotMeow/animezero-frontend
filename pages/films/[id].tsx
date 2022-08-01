6fbe6f8261f5c4a34ece47c18e950d00434c716f
ShotMeow <shotmeow@gmail.com>
2022-08-01 19:45:05 +0300

    Add Dynamic Page for Films

--- pages/films/[id].tsx
+++ pages/films/[id].tsx
@@ -1,7 +1,27 @@
-import { NextPage } from 'next'
+import { GetServerSideProps, NextPage } from 'next'
+import Film from '@/components/pages/Film/Film'
+import { ICurrentFilm, IFilm } from '@/services/films.interface'
+import { FilmsService } from '@/services/films.service'
 
-const FilmPage: NextPage = () => {
-    return <div></div>
+const FilmPage: NextPage<ICurrentFilm> = ({ film }) => {
+	return <Film film={film} />
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+	try {
+		const { data: film } = await FilmsService.getById(Number(query.id))
+		return {
+			props: {
+				film: film.data as IFilm
+			} as ICurrentFilm
+		}
+	} catch (e) {
+		return {
+			props: {
+				film: {} as IFilm
+			} as ICurrentFilm
+		}
+	}
 }
 
 export default FilmPage
