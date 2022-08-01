9235e30c01225d2202f1e22a93053fdda42eca15
ShotMeow <shotmeow@gmail.com>
2022-08-01 19:43:49 +0300

    Finish FilmBig Card

--- app/components/ui/FilmBig/FilmBig.tsx
+++ app/components/ui/FilmBig/FilmBig.tsx
@@ -4,28 +4,39 @@ import { IBigFilm } from '@/components/ui/FilmBig/FilmBig.interface'
 import styles from './FilmBig.module.scss'
 import Button from '@/components/ui/Button/Button'
 import Tag from '@/components/ui/Tag/Tag'
+import Link from 'next/link'
 
-const FilmBig: FC<IBigFilm> = ({ title, imageUrl, description, rating }) => {
+const FilmBig: FC<IBigFilm> = ({
+	id,
+	title,
+	imageUrl,
+	description,
+	rating
+}) => {
 	return (
-		<article
-			className={styles.film_big}
-			style={{ backgroundImage: `url(${imageUrl})` }}
-		>
-			<div className={styles.tags}>
-				<div>
-					<Tag title='Топ-100' />
-					<Tag title='Детям' />
-				</div>
-				<Tag title={rating} black={true} bold={true} />
-			</div>
-			<div className={styles.about}>
-				<div>
-					<h3>{title}</h3>
-					<p>{description}</p>
-				</div>
-				<Button>Смотреть</Button>
-			</div>
-		</article>
+		<Link href={`films/${id}`}>
+			<a>
+				<article
+					className={styles.film_big}
+					style={{ backgroundImage: `url(${imageUrl})` }}
+				>
+					<div className={styles.tags}>
+						<div>
+							<Tag title='Топ-100' />
+							<Tag title='Детям' />
+						</div>
+						<Tag title={rating} black={true} bold={true} />
+					</div>
+					<div className={styles.about}>
+						<div>
+							<h3>{title}</h3>
+							<p>{description}</p>
+						</div>
+						<Button important='primary'>Смотреть</Button>
+					</div>
+				</article>
+			</a>
+		</Link>
 	)
 }
 
