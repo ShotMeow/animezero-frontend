637c7a03809d5afcb6e1909f62eaef0effd1b2be
ShotMeow <shotmeow@gmail.com>
2022-08-03 21:50:58 +0300

    Add Progress Bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
 import '@/styles/globals.scss'
 import type { AppProps } from 'next/app'
+import NextProgressBar from 'nextjs-progressbar'
 
 function MyApp({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />
+	return (
+		<div>
+			<NextProgressBar
+				color='#643FFE'
+				startPosition={0.3}
+				stopDelayMs={200}
+				height={3}
+			/>
+			<Component {...pageProps} />
+		</div>
+	)
 }
 
 export default MyApp
