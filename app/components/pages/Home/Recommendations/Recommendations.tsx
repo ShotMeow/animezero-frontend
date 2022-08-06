962181c25d358174c73e1c0b4298fd3ee59a13a1
ShotMeow <shotmeow@gmail.com>
2022-08-06 22:07:51 +0300

    Finish Recommendations Section

--- app/components/pages/Home/Recommendations/Recommendations.tsx
+++ app/components/pages/Home/Recommendations/Recommendations.tsx
@@ -4,9 +4,9 @@ import { BiStar } from 'react-icons/bi'
 import Subtitle from '@/components/ui/Subtitle/Subtitle'
 import FilmBig from '@/components/ui/FilmBig/FilmBig'
 import FilmSmall from '@/components/ui/FilmSmall/FilmSmall'
-import { IBaseFilm } from '@/services/films.interface'
+import { IFilm } from '@/services/films.interface'
 
-const Recommendations: FC<{ recommended: IBaseFilm[] }> = ({ recommended }) => {
+const Recommendations: FC<{ films: IFilm[] }> = ({ films }) => {
 	return (
 		<section className={styles.recommendations}>
 			<header className={styles.heading}>
@@ -15,26 +15,9 @@ const Recommendations: FC<{ recommended: IBaseFilm[] }> = ({ recommended }) => {
 				</Subtitle>
 			</header>
 			<footer>
-				<FilmBig
-					id={recommended[0].id}
-					title={recommended[0].title}
-					description={recommended[0].description}
-					imageUrl={recommended[0].poster}
-					rating={recommended[0].rating}
-				/>
-
-				<FilmSmall
-					id={recommended[1].id}
-					title={recommended[1].title}
-					description={recommended[1].description}
-					imageUrl={recommended[1].poster}
-				/>
-				<FilmSmall
-					id={recommended[2].id}
-					title={recommended[2].title}
-					description={recommended[2].description}
-					imageUrl={recommended[2].poster}
-				/>
+				<FilmBig film={films[0]} />
+				<FilmSmall film={films[1]} />
+				<FilmSmall film={films[2]} />
 			</footer>
 		</section>
 	)
