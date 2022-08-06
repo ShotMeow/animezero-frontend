c764b61cc31a24243d9d8f02abdb7338f1081e3f
ShotMeow <shotmeow@gmail.com>
2022-08-06 22:07:29 +0300

    Finish Serials

--- app/components/pages/Serials/Serials.tsx
+++ app/components/pages/Serials/Serials.tsx
@@ -1,8 +1,36 @@
 import { FC } from 'react'
 import Layout from '@/components/Layout/Layout'
+import Heading from '@/components/ui/Heading/Heading'
+import { IFilm, IFilter } from '@/services/films.interface'
+import styles from './Serials.module.scss'
+import FilmItem from '@/components/ui/FilmItem/FilmItem'
+import { ILink } from '@/components/ui/Pagination/Pagination.interface'
+import Pagination from '@/components/ui/Pagination/Pagination'
+import Filter from '@/components/ui/Filter/Filter'
 
-const Serials: FC = () => {
-	return <Layout title='AnimeZero - Сериалы'></Layout>
+const Serials: FC<{ serials: IFilm[]; links: ILink[]; filters: IFilter }> = ({
+	serials,
+	links,
+	filters
+}) => {
+	return (
+		<Layout title='AnimeZero - Сериалы'>
+			<Heading
+				catalog='Сериалы'
+				title='Сериалы смотреть онлайн'
+				description='В нашем каталоге вы найдете сериалы любых жанров. Не упустите возможность смотреть сериалы онлайн бесплатно без регистрации.'
+			/>
+			<Filter filters={filters} />
+			<section className={styles.serials}>
+				<div className={styles.container}>
+					{serials.map(serial => (
+						<FilmItem film={serial} key={serial.id} />
+					))}
+				</div>
+				<Pagination links={links} />
+			</section>
+		</Layout>
+	)
 }
 
 export default Serials
--- pages/serials.tsx
+++ pages/serials.tsx
@@ -1,8 +1,55 @@
-import { NextPage } from 'next'
+import { GetServerSideProps, NextPage } from 'next'
 import Serials from '@/components/pages/Serials/Serials'
+import { FilmsService } from '@/services/films.service'
+import { IFilm, IFilter, IGenre, IStatus } from '@/services/films.interface'
+import { ILink } from '@/components/ui/Pagination/Pagination.interface'
 
-const SerialsPage: NextPage = () => {
-	return <Serials />
+const SerialsPage: NextPage<{
+	serials: IFilm[]
+	links: ILink[]
+	filters: IFilter
+}> = ({ serials, links, filters }) => {
+	return <Serials serials={serials} links={links} filters={filters} />
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+	const page = query.page || 1
+	const genre = query.genres
+	const status = query.statuses
+	const params: any = {
+		page: page,
+		genres: genre,
+		statuses: status
+	}
+	try {
+		const { data: serials } = await FilmsService.getAllByFilter({
+			...params
+		})
+		const { data: genres } = await FilmsService.getGenres()
+		const { data: statuses } = await FilmsService.getStatuses()
+
+		return {
+			props: {
+				serials: serials.data as IFilm[],
+				links: serials.meta.links as ILink[],
+				filters: {
+					genres: genres.data as IGenre[],
+					statuses: statuses.data as IStatus[]
+				}
+			} as { serials: IFilm[]; links: ILink[]; filters: IFilter }
+		}
+	} catch (e) {
+		return {
+			props: {
+				serials: [],
+				links: [],
+				filters: {
+					genres: [],
+					statuses: []
+				}
+			}
+		}
+	}
 }
 
 export default SerialsPage
