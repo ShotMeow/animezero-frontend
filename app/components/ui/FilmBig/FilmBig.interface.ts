d907f5ca51c22fa2426bad63bd28c4e3a18f1665
ShotMeow <shotmeow@gmail.com>
2022-08-06 22:14:31 +0300

    Finish Film Big

--- app/components/ui/FilmBig/FilmBig.interface.ts
+++
@@ -1,5 +0,0 @@
-import { IFilmSmall } from '@/components/ui/FilmSmall/FilmSmall.interface'
-
-export interface IBigFilm extends IFilmSmall {
-	rating: number
-}
--- app/components/ui/FilmBig/FilmBig.tsx
+++ app/components/ui/FilmBig/FilmBig.tsx
@@ -1,36 +1,29 @@
 import { FC } from 'react'
-import { IBigFilm } from '@/components/ui/FilmBig/FilmBig.interface'
-
 import styles from './FilmBig.module.scss'
 import Button from '@/components/ui/Button/Button'
 import Tag from '@/components/ui/Tag/Tag'
 import Link from 'next/link'
+import { IFilm } from '@/services/films.interface'
 
-const FilmBig: FC<IBigFilm> = ({
-	id,
-	title,
-	imageUrl,
-	description,
-	rating
-}) => {
+const FilmBig: FC<{ film: IFilm }> = ({ film }) => {
 	return (
-		<Link href={`films/${id}`}>
+		<Link href={`films/${film.id}`}>
 			<a>
 				<article
 					className={styles.film_big}
-					style={{ backgroundImage: `url(${imageUrl})` }}
+					style={{ backgroundImage: `url(${film.poster})` }}
 				>
 					<div className={styles.tags}>
 						<div>
 							<Tag title='Топ-100' />
 							<Tag title='Детям' />
 						</div>
-						<Tag title={rating} black={true} bold={true} />
+						<Tag title={film.rating} black={true} bold={true} />
 					</div>
 					<div className={styles.about}>
 						<div>
-							<h3>{title}</h3>
-							<p>{description}</p>
+							<h3>{film.title}</h3>
+							<p>{film.description}</p>
 						</div>
 						<Button important='primary'>Смотреть</Button>
 					</div>
