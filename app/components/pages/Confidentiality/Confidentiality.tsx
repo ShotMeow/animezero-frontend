e4bca5bcdd54aeebfcb1fe27823948707e816c01
ShotMeow <shotmeow@gmail.com>
2022-08-06 22:14:50 +0300

    Finish Confidentiality Page

--- app/components/pages/Confidentiality/Confidentiality.tsx
+++
@@ -1,8 +0,0 @@
-import { FC } from 'react'
-import Layout from '@/components/Layout/Layout'
-
-const Confidentiality: FC = () => {
-	return <Layout title='Политика конфиденциальности'></Layout>
-}
-
-export default Confidentiality
