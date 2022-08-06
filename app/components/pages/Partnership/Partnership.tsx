553028da8cb5df3e6c8ce5aa6f3ead6401d0c787
ShotMeow <shotmeow@gmail.com>
2022-08-06 22:09:29 +0300

    Finish Partnership Page

--- app/components/pages/Partnership/Partnership.tsx
+++ app/components/pages/Partnership/Partnership.tsx
@@ -1,8 +1,32 @@
 import { FC } from 'react'
 import Layout from '@/components/Layout/Layout'
+import Heading from '@/components/ui/Heading/Heading'
+import Button from '@/components/ui/Button/Button'
+import styles from './Partnership.module.scss'
 
 const Partnership: FC = () => {
-	return <Layout title='Сотрудничество'></Layout>
+	return (
+		<Layout title='AnimeZero - Сотрудничество'>
+			<Heading
+				catalog='Сотрудничество'
+				title='Партнерам'
+				description='Наш кинотеатр всегда рад новым партнерам. За поддержку нашего портала мы будем рады предоставить эксклюзивные предложения и надежные прозрачные взаимоотношения.'
+			/>
+			<div className={styles.partner}>
+				<p>
+					Получить более подробную информацию о партнерской программе вы можете
+					обратившись в чате:
+				</p>
+				<div>
+					<Button important='primary'>Поддержка</Button>
+					<p>
+						Или на почту:
+						<a href='mailto:admin@animezero.ru'>admin@animezero.ru</a>
+					</p>
+				</div>
+			</div>
+		</Layout>
+	)
 }
 
 export default Partnership
