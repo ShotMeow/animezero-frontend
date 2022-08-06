b8d722c1a013760cafc9d840f7cc4e4fdc385da0
ShotMeow <shotmeow@gmail.com>
2022-08-06 22:08:47 +0300

    Finish Privacy Page

--- pages/confidentiality.tsx
+++
@@ -1,7 +0,0 @@
-import { NextPage } from 'next'
-
-const ConfidentialityPage: NextPage = () => {
-	return <div></div>
-}
-
-export default ConfidentialityPage
---
+++ pages/privacy.tsx
@@ -0,0 +1,8 @@
+import { NextPage } from 'next'
+import Privacy from '@/components/pages/Privacy/Privacy'
+
+const PrivacyPage: NextPage = () => {
+	return <Privacy />
+}
+
+export default PrivacyPage
