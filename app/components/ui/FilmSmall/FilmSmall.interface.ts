13c95594da650ea711e6f3dd2450fc79b06b9ff0
ShotMeow <shotmeow@gmail.com>
2022-08-06 22:14:02 +0300

    Finish Film Small

--- app/components/ui/FilmSmall/FilmSmall.interface.ts
+++
@@ -1,6 +0,0 @@
-export interface IFilmSmall {
-	id: number
-	title: string
-	imageUrl: string
-	description: string
-}
--- app/components/ui/FilmSmall/FilmSmall.tsx
+++ app/components/ui/FilmSmall/FilmSmall.tsx
@@ -1,23 +1,22 @@
 import { FC } from 'react'
-import { IFilmSmall } from '@/components/ui/FilmSmall/FilmSmall.interface'
-
 import styles from './FilmSmall.module.scss'
 import Tag from '@/components/ui/Tag/Tag'
 import Link from 'next/link'
+import { IFilm } from '@/services/films.interface'
 
-const FilmSmall: FC<IFilmSmall> = ({ id, title, imageUrl, description }) => {
+const FilmSmall: FC<{ film: IFilm }> = ({ film }) => {
 	return (
-		<Link href={`films/${id}`}>
+		<Link href={`films/${film.id}`}>
 			<a>
 				<article className={styles.film_small}>
-					<div style={{ backgroundImage: `url(${imageUrl})` }} />
+					<div style={{ backgroundImage: `url(${film.poster})` }} />
 					<div className={styles.about}>
 						<div className={styles.tags}>
 							<Tag title='Топ-100' />
 							<Tag title='Детям' />
 						</div>
-						<h3>{title}</h3>
-						<p>{description}</p>
+						<h3>{film.title}</h3>
+						<p>{film.description}</p>
 					</div>
 				</article>
 			</a>
