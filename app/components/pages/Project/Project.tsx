0db9e5197b4f223d52d931bb1c4bc4ec79ff7da3
ShotMeow <shotmeow@gmail.com>
2022-08-06 22:08:11 +0300

    Finish Project Page

--- app/components/pages/Project/Project.tsx
+++ app/components/pages/Project/Project.tsx
@@ -1,8 +1,32 @@
 import { FC } from 'react'
 import Layout from '@/components/Layout/Layout'
+import Heading from '@/components/ui/Heading/Heading'
+import Image from 'next/image'
+import styles from './Project.module.scss'
+
+import quality from '@/assets//images/quality.png'
 
 const Project: FC = () => {
-	return <Layout title='О проекте'></Layout>
+	return (
+		<Layout title='AnimeZero - О проекте'>
+			<Heading
+				catalog='О проекте'
+				title='О нас'
+				description='AnimeZero - один из крупнейших онлайн-кинотеатров в России, посвященный аниме.'
+			/>
+			<div className={styles.project}>
+				<h3>Огромный выбор фильмов и сериалов</h3>
+				<p>
+					В нашем каталоге вы найдете огромную коллекцию аниме фильмов и
+					сериалов с большим выбором озвучки и хорошим качеством
+					воспроизведения. Для вас мы ежедневно работаем над улучшением нашего
+					сервиса, чтобы принести в каждый дом уютную атмосферу и удобный
+					просмотр аниме.
+				</p>
+				<Image src={quality} alt='Качество' />
+			</div>
+		</Layout>
+	)
 }
 
 export default Project
