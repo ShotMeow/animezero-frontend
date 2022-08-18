d4944d2fb14ca42a1a576fcd351c648c9f5838c1
ShotMeow <shotmeow@gmail.com>
2022-08-18 22:52:55 +0300

    Add Profile Endpoints

--- app/store/api/api.ts
+++ app/store/api/api.ts
@@ -2,10 +2,12 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/dist/query/react'
 import { API_URL } from '../../api/axios'
 import { TypeRootState } from '../store'
 import { IVerify } from '@/services/auth/auth.helper'
+import { IUser } from '@/types/user.interface'
+import { IFilm } from '@/services/films.interface'
 
 export const api = createApi({
 	reducerPath: 'api',
-	tagTypes: ['Auth', 'Profile'],
+	tagTypes: ['Auth', 'Profile', 'Films'],
 	baseQuery: fetchBaseQuery({
 		baseUrl: API_URL,
 		prepareHeaders: (headers, { getState }) => {
@@ -20,9 +22,66 @@ export const api = createApi({
 				`email/verify/${query.id}/${query.hash}?expires=${query.expires}&signature=${query.signature}`,
 			providesTags: () => [{ type: 'Auth' }]
 		}),
-		profile: builder.query({
+		profile: builder.query<IUser, any>({
 			query: () => `user/info`,
 			providesTags: () => [{ type: 'Profile' }]
+		}),
+		showWatchedFilms: builder.query<IFilm[], any>({
+			query: () => `film/watched`,
+			providesTags: () => [{ type: 'Films' }]
+		}),
+		addWatchedFilms: builder.mutation<void, number>({
+			query: film_id => ({
+				url: 'film/watched',
+				method: 'POST',
+				body: film_id
+			}),
+			invalidatesTags: ['Films']
+		}),
+		deleteWatchedFilms: builder.mutation<void, number>({
+			query: film_id => ({
+				url: `film/watched/${film_id}`,
+				method: 'DELETE'
+			}),
+			invalidatesTags: ['Films']
+		}),
+		showTrackedFilms: builder.query<IFilm[], any>({
+			query: () => `film/tracked`,
+			providesTags: () => [{ type: 'Films' }]
+		}),
+		addTrackedFilms: builder.mutation<void, number>({
+			query: film_id => ({
+				url: 'film/tracked',
+				method: 'POST',
+				body: film_id
+			}),
+			invalidatesTags: ['Films']
+		}),
+		deleteTrackedFilms: builder.mutation<void, number>({
+			query: film_id => ({
+				url: `film/tracked/${film_id}`,
+				method: 'DELETE'
+			}),
+			invalidatesTags: ['Films']
+		}),
+		showWantToWatchFilms: builder.query<IFilm[], any>({
+			query: () => `film/want-to-watch`,
+			providesTags: () => [{ type: 'Films' }]
+		}),
+		addWantToWatchFilms: builder.mutation<void, number>({
+			query: film_id => ({
+				url: 'film/want-to-watch',
+				method: 'POST',
+				body: film_id
+			}),
+			invalidatesTags: ['Films']
+		}),
+		deleteWantToWatchFilms: builder.mutation<void, number>({
+			query: film_id => ({
+				url: `film/want-to-watch/${film_id}`,
+				method: 'DELETE'
+			}),
+			invalidatesTags: ['Films']
 		})
 	})
 })
