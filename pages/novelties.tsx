94c5635b367c54958c9c7dd3c230d793aed7c506
storm <sahalisa17@gmail.com>
2022-09-07 19:00:51 +0300

    Novelties page refactoring

--- pages/novelties.tsx
+++ pages/novelties.tsx
@@ -1,28 +1,40 @@
-import { GetStaticProps, NextPage } from 'next'
-import Novelties from '@/app/components/pages/Novelties/Novelties'
 import { FilmsService } from '@/app/services/films.service'
 import { IFilm } from '@/app/services/films.interface'
+import Layout from '@/app/components/Layout/Layout'
+import Heading from '@/app/components/ui/Heading/Heading'
+import styles from '@/app/components/pages/Novelties/Novelties.module.scss'
+import FilmItem from '@/app/components/ui/FilmItem/FilmItem'
 
-const NoveltiesPage: NextPage<{ newest: IFilm[] }> = ({ newest }) => {
-	return <Novelties films={newest} />
+interface INoveltiesPageProps {
+	films: IFilm[]
 }
 
-export const getStaticProps: GetStaticProps = async () => {
+export default function NoveltiesPage(props: INoveltiesPageProps) {
+	return (
+		<Layout title='AnimeZero - Новинки'>
+			<Heading
+				catalog='Новинки'
+				title='Новинки'
+				description='На данной странице вы можете увидеть аниме, недавно появившиеся в нашем кинотеатре.'
+			/>
+			<section className={styles.films}>
+				{props.films?.map(film => <FilmItem film={film} key={film.id} />)}
+			</section>
+		</Layout>
+	)
+}
+
+export async function getStaticProps() {
 	try {
-		const { data: films } = await FilmsService.getAll(['newest'])
+		const films = await FilmsService.getAll(['newest'])
 		return {
 			props: {
-				newest: films.data.newest as IFilm[]
-			} as { newest: IFilm[] },
-			revalidate: 10
+				films: films.newest
+			}
 		}
 	} catch (e) {
 		return {
-			props: {
-				newest: []
-			}
+			notFound: true
 		}
 	}
 }
-
-export default NoveltiesPage
