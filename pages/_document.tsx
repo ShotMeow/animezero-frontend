6513f405dffdc0b7afc84d1aa606d58c213f6c53
ShotMeow <shotmeow@gmail.com>
2022-09-07 17:31:01 +0300

    Work to Optimize Code

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,7 +10,7 @@ const Document = () => {
 				<meta name='theme-color' content='#040404' />
 				<meta name='msapplication-navbutton-color' content='#643FFE' />
 				<meta name='apple-mobile-web-app-status-bar-style' content='#643FFE' />
-
+				<title>AnimeZero</title>
 				<link
 					href='https://fonts.googleapis.com/css2?family=Raleway&display=optional'
 					rel='stylesheet'
