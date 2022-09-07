060d3df1c23a5448e9ce8f6c7b0f68dddca16d09
ShotMeow <shotmeow@gmail.com>
2022-09-07 15:21:41 +0300

    Small Fixes

--- app/api/axios.ts
+++ app/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import { getContentType } from '@/utils/api.utils'
 
-export const API_URL = 'http://26.225.178.228:3000/api'
+export const API_URL = process.env.API_URL
 
 export const axiosClassic = axios.create({
 	baseURL: API_URL,
--- next.config.js
+++ next.config.js
@@ -2,7 +2,21 @@
 const nextConfig = {
 	reactStrictMode: true,
 	swcMinify: true,
-	poweredByHeader: false
+	poweredByHeader: false,
+	env: {
+		API_URL: process.env.API_URL
+	},
+	images: {
+		domains: ['st.kp.yandex.net']
+	},
+	async rewrites() {
+		return [
+			{
+				source: '/api/:path*',
+				destination: `${process.env.API_URL}/:path*`
+			}
+		]
+	}
 }
 
 module.exports = nextConfig
